f0bc402a2c37fe7a90babe0e0399a8c5aef00629
Chad Rutherford <chad_rutherford@outlook.com>
2020-04-02 13:20:37 -0400

    Initial Commit

---
+++ NeuMusicPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeuMusicPlayer
+//
+//  Created by Chad Rutherford on 4/2/20.
+//  Copyright © 2020 Chad Rutherford. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
